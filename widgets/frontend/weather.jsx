9f012d08d44c169b023ff920f6834f19b9e271a6
atar97 <austin.cotant@gmail.com>
2018-09-28 17:04:48 -0700

    Add ajax call to weather API

--- widgets/frontend/weather.jsx
+++ widgets/frontend/weather.jsx
@@ -11,11 +11,35 @@ class Weather extends React.Component {
 
   getWeather(city) {
     $.ajax({
-      url: `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3b865174016d7ba667d628015d46ae84`,
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3b865174016d7ba667d628015d46ae84`,
       method: "GET",
       dataType: "JSONP"
-    }).then((res) => console.log(res));
+    }).then((res) => {
+      console.log(res.weather);
+      console.log(this);
+    });
+  //
+  // let xmlhttp = new XMLHttpRequest();
+  //
+  //     xmlhttp.onreadystatechange = function() {
+  //         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+  //            if (xmlhttp.status == 200) {
+  //                console.log(xmlhttp.response);
+  //            }
+  //            else if (xmlhttp.status == 400) {
+  //               alert('There was an error 400');
+  //            }
+  //            else {
+  //                alert('something else other than 200 was returned');
+  //            }
+  //         }
+  //     };
+  //
+  //     xmlhttp.open("GET", `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3b865174016d7ba667d628015d46ae84`, true);
+  //     xmlhttp.send();
+  //
   }
+
 }
 
 export default Weather;
